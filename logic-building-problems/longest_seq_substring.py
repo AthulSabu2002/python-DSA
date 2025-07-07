82b66956a272db9c7e309ba670efc9ccd90c30d6
athulsabu <athulsabu75@gmail.com>
2025-07-07 15:17:17 +0530

    update: length of longest substring

--- logic-building-problems/longest_seq_substring.py
+++ logic-building-problems/longest_seq_substring.py
@@ -1,10 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        subStr = ""
+        char_set = set()
+        max_len = 0
+        left = 0
         
-        
-        return subStr
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            
+            char_set.add(s[right])
 
+            max_len = max(max_len, right - left + 1)
+        
+        return max_len
 
 if __name__ == "__main__":
     obj = Solution()
