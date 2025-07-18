5039751d5437a6ed204720d333b1505f24fc22a7
athulsabu <athulsabu75@gmail.com>
2025-07-18 11:08:25 +0530

    added

--- dynamicProgramming/minCostClimbingStairs.py
+++ dynamicProgramming/minCostClimbingStairs.py
@@ -2,12 +2,20 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        
-        return
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1],
+                        dp[i - 2] + cost[i - 2])
+
+        return dp[n] 
     
     
 if __name__ == "__main__":
     obj = Solution()
     
-    print(obj.minCostClimbingStairs(cost = [1,2,3]))
+    print(obj.minCostClimbingStairs(nums = [1,2,3]))
